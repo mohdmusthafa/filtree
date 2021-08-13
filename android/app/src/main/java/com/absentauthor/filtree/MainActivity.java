bda8354c9222c9ded3dd511c06e05a3b96b3aa75
Muhamemd Musthafa <muhammed.mustafa@emvigotech.com>
2021-08-13 22:22:25 +0530

    ios bundle info fix

--- android/app/src/main/java/com/absentauthor/filtree/MainActivity.java
+++ android/app/src/main/java/com/absentauthor/filtree/MainActivity.java
@@ -1,4 +1,4 @@
-package com.filtree;
+package com.absentauthor.filtree;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "filtree";
+    return "FILTREE";
   }
 }
